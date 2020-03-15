e7faf6fd94f47a6e000f0479be432cf5ac6ab274
sania <grani.36985@gmail.com>
2020-03-15 11:06:46 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const mongoose = require("mongoose")
+
+const app = express()
+app.use(express.json({extended:true}))
+
+
+const PORT = 5000
+
+async function start(){
+    try{
+       
+        await mongoose.connect('mongodb://localhost:27017/app', {
+            useNewUrlParser:true,
+            useUnifiedTopology:true,
+            useCreateIndex:true
+        })
+
+        app.listen(PORT, ()=>console.log(`App has been started on port ${PORT}...`))
+    }catch(e){
+        console.log("Server error", e.message)
+        process.exit(1)
+    }
+}
+
+start()
